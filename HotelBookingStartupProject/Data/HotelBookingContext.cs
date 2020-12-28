9c5d917a18d334e6a738c0dadf879aadf2450c29
robie sun <robie.sun.rs@gmail.com>
2020-12-28 17:01:20 +0100

    first commit

---
+++ HotelBookingStartupProject/Data/HotelBookingContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelBookingStartupProject.Models;
+
+namespace HotelBookingStartupProject.Data
+{
+    public class HotelBookingContext : DbContext
+    {
+        public HotelBookingContext (DbContextOptions<HotelBookingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Booking> Booking { get; set; }
+
+        public DbSet<Room> Room { get; set; }
+
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HotelBookingStartupProject
+Initial and intentionally not very well designed ASP.NET Core MVC hotel booking application.
