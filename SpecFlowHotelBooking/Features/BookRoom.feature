b1a87ce506edfd9539407936b1d2ae63b63e5487
robie sun <robie.sun.rs@gmail.com>
2020-12-30 12:50:19 +0100

    first set up for specflow

---
+++ SpecFlowHotelBooking/Features/BookRoom.feature
@@ -0,0 +1,17 @@
+﻿Feature: BookRoom
+	As a potential customer 
+	I want to see which dates are booked
+	So I can decide what days are free to book a room.
+
+@mytag
+Scenario Outline:
+	Given the start date which is tomorrow plus <startDate>
+	And the end date which is tomorrow plus <endDate>
+	When the dates are check with the occupied range
+	Then the <Availability> should be returned
+
+Examples: 
+| startDate | endDate | Availability |
+|	  0		|	 1    |    True      |
+
+
---
+++ SpecFlowHotelBooking/Steps/BookRoomSteps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowHotelBooking.Steps
+{
+    [Binding]
+    public sealed class BookRoomSteps   
+    {
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public BookRoomSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [Given(@"the start date which is tomorrow plus (.*)")]
+        public void GivenTheStartDateWhichIsTomorrowPlus(int p0)
+        {
+            _scenarioContext.Pending();
+        }
+        
+        [Given(@"the end date which is tomorrow plus (.*)")]
+        public void GivenTheEndDateWhichIsTomorrowPlus(int p0)
+        {
+            _scenarioContext.Pending();
+        }
+        
+        [When(@"the dates are check with the occupied range")]
+        public void WhenTheDatesAreCheckWithTheOccupiedRange()
+        {
+            _scenarioContext.Pending();
+        }
+        
+        [Then(@"the True should be returned")]
+        public void ThenTheTrueShouldBeReturned()
+        {
+            _scenarioContext.Pending();
+        }
+    }
+}
