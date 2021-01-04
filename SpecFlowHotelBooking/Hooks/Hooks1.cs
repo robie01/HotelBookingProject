5e6213e171bf5863a91f9584dc653be1c6ec75cf
SamPerCap <31726611+SamPerCap@users.noreply.github.com>
2021-01-04 10:28:37 +0100

    BeforeScenario BeforeTestRun

---
+++ SpecFlowHotelBooking/Hooks/Hooks1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowHotelBooking
+{
+    [Binding]
+    public sealed class Hooks1
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        [BeforeScenario]
+        public static void BeforeScenario()
+        {
+            //TODO: implement logic that has to run before executing each scenario
+            
+            //Get all rooms from based on customer's wish
+            //Room 1 and 2 are occupied but 3 and 4 free
+        }
+
+        [BeforeTestRun]
+        public void BeforeStart()
+        {
+            //Offer a higher quality room
+
+        }
+    }
+}
